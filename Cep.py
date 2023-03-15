452b4c4b724b19e8c758ff86149f715a279bdfdb
Eduarda Costa <eduarda.schuler.costa@gmail.com>
2023-03-14 22:22:07 -0300

    updates

---
+++ Cep.py
@@ -0,0 +1,19 @@
+import requests
+class Cep():
+    def __init__(self, cep):
+        self.cep = cep
+
+    #Methods
+    def busca_padrao_cep(self):
+        pass
+        #o usuário pode inserir o CEP com a formatação ou não
+
+    def requisicao_via_cep(self):
+        pass
+
+    def valida_cep(self):
+        pass
+        #verifica se o formato de dado inserido é valido para um CEP        
+
+    def formata_cep(self):
+        pass
