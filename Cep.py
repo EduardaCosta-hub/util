5496f1aa09277b2e51c45c4fd1111f93400bcc16
Eduarda Costa <eduarda.schuler.costa@gmail.com>
2023-03-15 22:03:09 -0300

    updates

--- Cep.py
+++ Cep.py
@@ -1,16 +1,26 @@
 import requests
+import re
 class Cep():
     def __init__(self, cep):
         self.__cep = cep
         self.__rua = ""
         self.__cidade = ""
         self.__estado = ""
-        self.__pais = self.pais()
+        self.__pais = "Brasil"
+        self.encontra_padrao_cep()
     #Methods
-    def busca_padrao_cep(self):
-        pass
+    def encontra_padrao_cep(self):
         #o usuário pode inserir o CEP com a formatação ou não
-
+        padrao = '[0-9]{5}(-)?[0-9]{3}'
+        encontra = re.search(padrao, self.__cep)
+        print(encontra.group())
+        pass
+        
+    def higieniza_cep(self, cep):
+        cep = str(cep)
+        cep = cep.strip()
+        return cep
+        
     def requisicao_via_cep(self):
         pass
         #busca o cep em https://viacep.com.br/
@@ -26,8 +36,3 @@ class Cep():
     def extrai_informacoes_cep(self):
         pass
         #separa os dados do json retornado pela via cep entre os atributos da classe
-
-    @staticmethod
-    def pais(self):
-        pais = "Brasil"
-        return pais
---
+++ main.py
@@ -0,0 +1,3 @@
+from Cep import Cep
+
+cep1 = Cep('meu cep é 12345-678 asldfsa 12345110')
