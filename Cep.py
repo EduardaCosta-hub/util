acfcc4d89cc03fc7c92d050f4e79f6e3a2e4a91e
Eduarda Schüler Costa <eduarda.schuler.costa@gmail.com>
2023-03-20 12:49:35 -0300

    updatea

--- Cep.py
+++ Cep.py
@@ -1,27 +1,33 @@
-import requests
-import re
+import re, requests
 class Cep():
     def __init__(self, cep):
-        self.__cep = self.busca_padrao_cep(cep)
+        self.__cep = self.higieniza_cep(cep)
         self.__rua = ""
         self.__cidade = ""
         self.__estado = ""
         self.__pais = "Brasil"
+        
     #Methods
     def busca_padrao_cep(self, cep):
-        #o usuário pode inserir o CEP com a formatação ou não
-        padrao = '[0-9]{5}(-)?[0-9]{3}'
+        padrao = '[0-9]{5}(-)?[0-9]{3}' #o usuário pode inserir o CEP como 12345678 ou 12345-678
         encontra = re.search(padrao, cep)
         if not encontra:
             raise ValueError("Nenhum formato válido de CEP encontrado! Formatos válidos: 12345678 ou 12345-678.")
         else:
             cep = encontra.group()
         return cep
-        
+    
+    def higieniza_cep(self, cep):
+        cep = self.busca_padrao_cep(cep)
+        cep = cep.replace('-','') #deixa o CEP no formato 12345678 
+        return cep
+    
     def requisicao_via_cep(self):
-        pass
         #busca o cep em https://viacep.com.br/
-
+        url = f'https://viacep.com.br/ws/{self.__cep}/json/'
+        req = requests.get(url)
+        print(req.json())
+        
     def valida_cep(self):
         pass
         #verifica se o formato de dado inserido é valido para um CEP        
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Cep import Cep
 
-cep1 = Cep('meu cep é 12345678 asldfsa 1234e5110')
-print(cep1.cep)
+cep2 = Cep('meu cep é 93700000 asldfsa 1234e5110')
+print(cep2.cep)
+cep2.requisicao_via_cep()
