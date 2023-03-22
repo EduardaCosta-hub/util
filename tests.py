33d7f50b5fdd00b4820bd6c8f908103d7bbd00f2
Eduarda Costa <eduarda.schuler.costa@gmail.com>
2023-03-21 21:33:07 -0300

    updates

---
+++ tests.py
@@ -0,0 +1,11 @@
+import pytest
+from Cep import Cep
+class TestClass():
+    def test_busca_padrao_cep_recebe_palvraaletaoria01001000_devolve_01001000(self):
+        entrada = 'palavraaleatoria 01001000'
+        esperado = '01001000'
+
+        cep_teste = Cep(entrada)
+        resultado = cep_teste.busca_padrao_cep()
+
+        assert resultado == esperado
