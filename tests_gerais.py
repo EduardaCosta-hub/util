cbe49a19ccf8c88cdd49054da5a49f6c428065c1
Eduarda Costa <eduarda.schuler.costa@gmail.com>
2023-03-21 22:03:23 -0300

    updates

--- tests_gerais.py
+++ tests_gerais.py
@@ -4,8 +4,6 @@ class TestClass():
     def test_busca_padrao_cep_recebe_palvraaletaoria01001000_devolve_01001000(self):
         entrada = 'palavraaleatoria 01001000'
         esperado = '01001000'
-
         cep_teste = Cep(entrada)
-        resultado = cep_teste.busca_padrao_cep()
-
+        resultado = cep_teste.busca_padrao_cep(entrada)
         assert resultado == esperado
