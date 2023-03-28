aaa2cbdc98646664955fbfc53622a9626fa735cb
Eduarda Costa <eduarda.schuler.costa@gmail.com>
2023-03-28 20:50:55 -0300

    updates

--- test_class_cep.py
+++ test_class_cep.py
@@ -29,66 +29,57 @@ class TestClass:
     
     def test_extrai_informacao_cep_recebe_01001000_devolve_cep_formatado_01001000(self):
         entrada = '01001000'
-        cep_formatado_esperado = '01001-000'
+        esperado = '01001-000'
 
-        cep_teste = Cep('01001000')
+        cep_teste = Cep(entrada)
         cep_teste.extrai_informacoes_cep()
 
         resultado_cep_formatado = cep_teste.cep_formatado
 
-        assert resultado_cep_formatado == cep_formatado_esperado
+        assert resultado_cep_formatado == esperado
 
     def test_extrai_informacao_cep_recebe_01001000_devolve_logradouro_praca_da_se(self):
         entrada = '01001000'
-        logradouro_esperado = 'Praça da Sé'
-
-        cep_teste = Cep('01001000')
+        esperado = 'Praça da Sé'
+        cep_teste = Cep(entrada)
         cep_teste.extrai_informacoes_cep()
-
-        resultado_logradouro = cep_teste.logradouro
-
-        assert resultado_logradouro == logradouro_esperado
+        resultado = cep_teste.logradouro
+        assert resultado == esperado
 
     def test_extrai_informacao_cep_recebe_01001000_devolve_complemento_lado_impar(self):
         entrada = '01001000'
-        complemento_esperado = 'lado ímpar'
-
-        cep_teste = Cep('01001000')
+        esperado = 'lado ímpar'
+        cep_teste = Cep(entrada)
         cep_teste.extrai_informacoes_cep()
-
-        resultado_complemento = cep_teste.complemento
-
-        assert resultado_complemento == complemento_esperado
+        resultado = cep_teste.complemento
+        assert resultado == esperado
 
     def test_extrai_informacao_cep_recebe_01001000_devolve_bairro_se(self):
         entrada = '01001000'
-        bairro_esperado = 'Sé'
-
-        cep_teste = Cep('01001000')
+        esperado = 'Sé'
+        cep_teste = Cep(entrada)
         cep_teste.extrai_informacoes_cep()
-
-        resultado_bairro = cep_teste.bairro
-
-        assert resultado_bairro == bairro_esperado
+        resultado = cep_teste.bairro
+        assert resultado == esperado
 
     def test_extrai_informacao_cep_recebe_01001000_devolve_localidade_sao_paulo(self):
         entrada = '01001000'
-        localidade_esperada = 'São Paulo'
-
-        cep_teste = Cep('01001000')
+        esperado = 'São Paulo'
+        cep_teste = Cep(entrada)
         cep_teste.extrai_informacoes_cep()
-
-        resultado_localidade = cep_teste.localidade
-
-        assert resultado_localidade == localidade_esperada
+        resultado = cep_teste.localidade
+        assert resultado == esperado
 
     def test_extrai_informacao_cep_recebe_01001000_devolve_uf_sp(self):
         entrada = '01001000'
-        uf_esperada = 'SP'
-
-        cep_teste = Cep('01001000')
+        esperado = 'SP'
+        cep_teste = Cep(entrada)
         cep_teste.extrai_informacoes_cep()
-        
-        resultado_uf = cep_teste.uf
+        resultado = cep_teste.uf
+        assert resultado == esperado
 
-        assert resultado_uf == uf_esperada
+    def test_extrai_informacao_cep_recebe_12345678_devolve_erro(self):
+        with pytest.raises(Exception):
+            entrada = '12345678'
+            cep_teste = Cep(entrada)
+            cep_teste.extrai_informacoes_cep()
