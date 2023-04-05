b8f423fcc1cda8ff84fa50ddbe1fc73596b07582
Eduarda Costa <eduarda.schuler.costa@gmail.com>
2023-04-04 21:12:47 -0300

    updates

--- test_class_endereco.py
+++ test_class_endereco.py
@@ -13,3 +13,12 @@ class TestClass():
             resultado = endereco_teste.logradouro
         assert resultado == esperado
     
+    def test_verifica_numero_recebe_cep_9370000_pede_numero_e_recebe_999(self):
+        endereco_teste = Endereco(93700000)
+        recebe = 999
+        esperado = 999
+        with MonkeyPatch.context() as monkeypatch:
+            monkeypatch.setattr(builtins, 'input', lambda _: recebe)
+            endereco_teste.verifica_numero()
+            resultado = endereco_teste.numero
+        assert resultado == esperado
